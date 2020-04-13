ed5b7c50876b8c9a68e932c1147b05d16e720999
madhuvayya <madhu.vayya@gmail.com>
2020-04-13 14:24:37 +0530

    Added for loop to calculate month salary

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,21 +2,28 @@
 
 echo "Welcome to Employee Wage Computation"
 
-isPartTime=1
-isFullTime=2
-empRatePerHr=20
+IS_PART_TIME=1
+IS_FULL_TIME=2
+EMP_RATE_PER_HR=20
+NUM_WORKING_DAYS=20
 
-random=$(($RANDOM%3))
-case $random in
-	$isFullTime )
-		empHrs=8
-		;;
-	$isPartTime )
-		empHrs=4
-		;;
-	* )
-		empHrs=0
-		;;
-esac
+totalSalary=0
 
-salary=$(($empHrs*$empRatePerHr))
+for((i=0;i<$NUM_WORKING_DAYS;i++))
+do
+	random=$(($RANDOM%3))
+	case $random in
+		$IS_FULL_TIME )
+			empHrs=8
+			;;
+		$IS_PART_TIME )
+			empHrs=4
+			;;
+		* )
+			empHrs=0
+			;;
+	esac
+
+	salary=$(($empHrs*$EMP_RATE_PER_HR))
+	totalSalary=$(($totalSalary+$salary))
+done
